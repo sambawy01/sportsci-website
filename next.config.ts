45ef6fa5901abc48c4b2e5c94914b95821cda202
sambawy01 <sambawy@gmail.com>
2026-03-25 04:37:50 +0200

    fix: remove basePath for custom domain

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export",
-  basePath: "/sportsci-website",
   images: {
     unoptimized: true,
   },
